9188cf8adbd613140c32c3641efd8def4a8820cb
Kasaiatsuki <kasaiatsuki@gmail.com>
2026-03-12 17:58:12 +0900

    feat: implement 288x288 center crop and update network for 69696 input units

--- e2e_nav_box1/network.py
+++ e2e_nav_box1/network.py
@@ -26,8 +26,8 @@ class Network(nn.Module):
         # 全結合層(Fully Connected Layer)の前に2次元のテンソルを1次元に平坦化(Flatten)するための層
         self.flatten = nn.Flatten()
         
-        # 全結合層1: 入力数128832 (512x288画像の場合の平坦化サイズ: 64*61*33)
-        self.fc1 = nn.Linear(128832, 512)
+        # 全結合層1: 入力数69696 (288x288クロップ画像の場合の平坦化サイズ: 64*33*33)
+        self.fc1 = nn.Linear(69696, 512)
         
         # 全結合層2(出力層): 角速度出力
         self.fc2 = nn.Linear(512, 1)
